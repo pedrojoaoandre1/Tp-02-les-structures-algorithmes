9af89a539d1db44c1e438acebbde0aea86267568
pandre4 <pandre4@350028R-F115X04.lise.kreizenn.bzh>
2026-09-25 12:05:33 +0200

    Ex02 Tp-02

---
+++ src/tp02/les_strucutre_algorithmique/Tp02Les_strucutre_algorithmique.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp02.les_strucutre_algorithmique;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pandre4
+ */
+public class Tp02Les_strucutre_algorithmique {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                
+        //Ex 6 Opérations arithmétiques sur entiers
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Quel est le mot de passe: ");
+        String motdepasse = sc.nextLine();
+
+        while(!motdepasse.equals("admin123")){
+            System.out.println("Mot de passe incorrect, veuillez réessayer:");
+            motdepasse = sc.nextLine();
+        }
+        System.out.println("Utilisateur valide");
+
+    }
+    
+}
